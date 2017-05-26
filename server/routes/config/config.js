db28ce4e41fcbff7e3a5193576fcec0a464fbdea
mrabanda <mrabanda@gmail.com>
2017-05-26 03:22:23 -0400

    google callback mod

--- server/routes/config/config.js
+++ server/routes/config/config.js
@@ -2,6 +2,6 @@ module.exports = {
   'googleAuth': {
     'clientID': '188696954394-mpr4mcb5prpku3edvd733atu3uc58pcn.apps.googleusercontent.com',
     'clientSecret': 'Fcf0xUINliqt2CflT0Bb2lGh',
-    'callbackURL': 'https://foodfriendsapp/auth/google/callback'
+    'callbackURL': 'https://foodfriendsapp.herokuapp.com/auth/google/callback'
   }
 }
