1458a1ba71f64505d9a1d392d9f90754ea13d09e
Abanda <mrabanda@users.noreply.github.com>
2017-05-26 03:38:33 -0400

    Delete config.js

--- server/routes/config/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  'googleAuth': {
-    'clientID': '188696954394-mpr4mcb5prpku3edvd733atu3uc58pcn.apps.googleusercontent.com',
-    'clientSecret': 'Fcf0xUINliqt2CflT0Bb2lGh',
-    'callbackURL': 'https://foodfriendsapp.herokuapp.com/auth/google/callback'
-  }
-}
