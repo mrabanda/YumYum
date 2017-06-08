0c109bf434df80df21aebafe1c6d164d15aa5fed
mrabanda <mrabanda@gmail.com>
2017-06-08 04:06:55 -0400

    added config files

---
+++ server/routes/config/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  'googleAuth': {
+    'clientID': '188696954394-mpr4mcb5prpku3edvd733atu3uc58pcn.apps.googleusercontent.com',
+    'clientSecret': 'Fcf0xUINliqt2CflT0Bb2lGh',
+    'callbackURL': 'https://foodfriendsapp.herokuapp.com/auth/google/callback'
+  }
+}
